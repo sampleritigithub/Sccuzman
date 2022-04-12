8bf5cb0000949da74dc84e65a89277e8c7c1f0a9
Omar Ismail_Source <103457750+sampleritigithub@users.noreply.github.com>
2022-04-12 15:38:55 +0200

    Create main.py
    Main program

---
+++ main.py
@@ -0,0 +1,83 @@
+print("""
+Sccuzman interface
+==================
+1. Run
+2. cmd
+3. Make
+==================
+""")
+option = input("<<<Option>>>")
+if option == "1":
+  import os
+  os.startfile("Optionrun.py")
+if option == "2":
+  import os
+  cmd = input("C:\Windows\System32")
+  os.system(cmd)
+if option == "3"
+  print("""
+  Sccuzman Editor
+  ===============
+  Max number of lines: 10 
+  Name:
+  """")
+  filename = input("")
+  print("Ok, ready to edit:", filename)
+  print("Write: oksdoijoidjojdojdoixjoijcoidxjojik if you want to finish")
+  first_line = input("")
+  if first_line == "oksdoijoidjojdojdoixjoijcoidxjojik":
+    with open(filename, "w")as f:
+      f.writelines()
+  else:
+    secline = input("")
+    if secline == "oksdoijoidjojdojdoixjoijcoidxjojik":
+      with open(filename, "w")as f:
+        f.writelines(first_line)
+    else:
+      line3 = input("")
+      if line3 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+        with open(filename, "w")as f:
+          f.writelines(first_line + "\n" + secline + "\n"")
+      else:
+          line4 = input("")
+          if line4 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+            with open(filename, "w")as f:
+              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n"")
+          else:
+                line5 = input("")
+                if line5 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                  with open(filename, "w")as f:
+                    f.writelines(first_line + "\n" + secline + "\n" +  line3 + "\n" +  line4 + "\n"")
+                else:
+                  line6 = input("")
+                  if line6 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                    with open(filename, "w")as f:
+                      f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n"")
+                  else:
+                    line7 = input("")
+                    if line7 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                      with open(filename, "w")as f:
+                        f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n"")
+                    else:
+                      line8 = input("")
+                      if line8 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                        with open(filename, "w")as f:
+                          f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n"")
+                      else:
+                        line9 = input("")
+                        if line9 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                          with open(filename, "w")as f:
+                            f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n"")
+                        else:
+                          line10 = input("")
+                          if line10 == "oksdoijoidjojdojdoixjoijcoidxjojik":
+                            with open(filename, "w")as f:
+                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n"")
+                          else:
+                            with open(filename, "w")as f:
+                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n" + line10 + "\n"")
+                        
+                          
+                    
+          
+      
