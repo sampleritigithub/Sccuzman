603350abb0a4fe78f4f66c4efdf55eadd29eba70
Tobey_Source <103457750+sampleritigithub@users.noreply.github.com>
2022-04-30 08:27:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,45 +37,45 @@ if option == "3"
       line3 = input("")
       if line3 == "oksdoijoidjojdojdoixjoijcoidxjojik":
         with open(filename, "w")as f:
-          f.writelines(first_line + "\n" + secline + "\n"")
+          f.writelines(first_line + "\n" + secline + "\n")
       else:
           line4 = input("")
           if line4 == "oksdoijoidjojdojdoixjoijcoidxjojik":
             with open(filename, "w")as f:
-              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n"")
+              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n")
           else:
                 line5 = input("")
                 if line5 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                   with open(filename, "w")as f:
-                    f.writelines(first_line + "\n" + secline + "\n" +  line3 + "\n" +  line4 + "\n"")
+                    f.writelines(first_line + "\n" + secline + "\n" +  line3 + "\n" +  line4 + "\n")
                 else:
                   line6 = input("")
                   if line6 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                     with open(filename, "w")as f:
-                      f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n"")
+                      f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n" +  line5 + "\n")
                   else:
                     line7 = input("")
                     if line7 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                       with open(filename, "w")as f:
-                        f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n"")
+                        f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n"+  line5 + "\n" + line6 + "\n")
                     else:
                       line8 = input("")
                       if line8 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                         with open(filename, "w")as f:
-                          f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n"")
+                          f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n" +  line5 + "\n" + line6 + "\n" + line7 + "\n")
                       else:
                         line9 = input("")
                         if line9 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                           with open(filename, "w")as f:
-                            f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n"")
+                            f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n" +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n")
                         else:
                           line10 = input("")
                           if line10 == "oksdoijoidjojdojdoixjoijcoidxjojik":
                             with open(filename, "w")as f:
-                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n"")
+                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n" +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n")
                           else:
                             with open(filename, "w")as f:
-                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n" + line10 + "\n"")
+                              f.writelines(first_line + "\n" + secline + "\n" + line3 + "\n" +  line4 + "\n" +  line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9 + "\n" + line10 + "\n")
                         
                           
                     
